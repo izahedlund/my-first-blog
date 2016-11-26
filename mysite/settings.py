b77ddf0698c486615770e4d62ebfff10339e5eab
izahedlund <izahed@edu.umea.se>
2016-11-26 11:16:26 +0100

    message

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+ue6_1=gnifzz+kwjwc=b%xnk-77z(!&2m5t2c*)f33os7qr$n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'izahedlund.pythonanywhere.com']
 
 
 # Application definition
